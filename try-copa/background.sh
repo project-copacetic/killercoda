23ffe5974c3b9d47a58fdb524ac3fae2b24740a6
Xander Grzywinski <xandergrzyw@gmail.com>
2023-12-05 13:02:35 -0800

    grab latest copa release

--- try-copa/background.sh
+++ try-copa/background.sh
@@ -3,8 +3,13 @@
 apt-get update
 apt-get install -y wget 
 
-wget https://github.com/project-copacetic/copacetic/releases/download/v0.4.1/copa_0.4.1_linux_amd64.tar.gz
-tar -xzf copa_0.4.1_linux_amd64.tar.gz
+curl -s https://api.github.com/repos/project-copacetic/copacetic/releases/latest \
+| grep "browser_download_url.*linux_amd64.tar.gz" \
+| grep -v -w "sbom" \
+| cut -d : -f 2,3 \
+| tr -d \" \
+| wget -qi -
+tar -xzf copa*
 mv copa /usr/local/bin/copa
 
 snap install trivy
